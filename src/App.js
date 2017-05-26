35874b41dea934fff5a609125e03c3e5dd936aab
Denis Nazarenko <dnazarex@gmail.com>
2017-05-26 12:42:23 +0300

    Created state, updated view

--- src/App.js
+++ src/App.js
@@ -3,16 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          name: 'Learn English',
+          isComplete: false
+        },
+        {
+          id: 2,
+          name: 'Learn JavaScript',
+          isComplete: true
+        },
+        {
+          id: 3,
+          name: 'Learn Spanish',
+          isComplete: true
+        }
+      ]
+    };
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Todos</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Todo-App">
+          <form>
+            <input type="text" />
+          </form>
+          <div className="Todo-List">
+            <ul>
+              {this.state.todos.map(todo => 
+                <li key={todo.id}>
+                  <input type="checkbox" defaultChecked={todo.isComplete}/> {todo.name}
+                </li>)}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
